0becf9a86d7aca911632138a6c21dc52b5775dd4
Yuchen He <he34@illinois.edu>
2017-11-25 17:08:11 -0600

    rename files

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import docker
+import yaml
 
 def check_docker():
     client = docker.from_env()
     print(client.ping())
-   
+
+
 def create_main_container():
     client = docker.APIClient(
         **docker.utils.kwargs_from_env(assert_hostname=False)
     )
-    container = client.create_container("ubuntu:trusty", "/bin/sleep 300000", detach=True, ports=[10000], name="c1")
+    container = client.create_container(
+        "ubuntu:trusty", "/bin/sleep 300000", detach=True, ports=[10000], name="c1")
     client.start(container=container.get('Id'))
     print("c1 created")
 
@@ -19,14 +22,22 @@ def create_children_container():
     )
 
     host_config = client.create_host_config(
-        links=[( "c1", "c1")]
+        links=[("c1", "c1")]
     )
 
-    container = client.create_container("ubuntu:trusty", "sh -c \"ping $C1_PORT_10000_TCP_ADDR\"", detach=True, host_config=host_config, name="c2")
+    container = client.create_container(
+        "ubuntu:trusty", "sh -c \"ping $C1_PORT_10000_TCP_ADDR\"", detach=True, host_config=host_config, name="c2")
     client.start(container=container.get('Id'))
     print("c2 created")
 
+
 def main(args=None):
+
+     # otherwise, try the default paths
+    for path in ("blockade.yaml", "blockade.yml"):
+        with open(path) as f:
+            d = yaml.safe_load(f)
+            return 
     check_docker()
     create_main_container()
     create_children_container()
