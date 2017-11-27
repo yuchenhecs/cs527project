65cf6d1b8be3d646fe7ec2c2c60acccce25d7780
Yuchen He <he34@illinois.edu>
2017-11-27 13:59:32 -0600

    add tests for image, volume, cmd

--- test/test_command.py
+++ test/test_command.py
@@ -4,15 +4,39 @@ import docker
 from src import main as core
 
 class CommandTests(unittest.TestCase):
+
+    IsSetup = False
+    inspect_c1 = None
+
     def setUp(self):
+        if not self.IsSetup:
+            print("Initializing testing environment")
+            # run the real setup
+            self.setupClass()
+            # remember that it was setup already
+            self.__class__.IsSetup = True
+
+
+    def setupClass(self):
         core.process_yaml("blockade.yaml")
-        
-    def test_create(self):
         client = docker.APIClient(
             **docker.utils.kwargs_from_env(assert_hostname=False)
         )
-        inspect = client.inspect_container("c1")
-        self.assertEqual(inspect["Config"]["Image"], "ubuntu:trusty")
+        self.__class__.inspect_c1 = client.inspect_container("c1")
+        
+        
+    def test_image(self):
+        self.assertEqual(self.__class__.inspect_c1["Config"]["Image"], "ubuntu:trusty")
+    
+    def test_cmd(self):
+        self.assertEqual(" ".join(self.__class__.inspect_c1["Config"]["Cmd"]), "/bin/sleep 300000")
+
+    def test_volume(self):
+        self.assertEqual(self.__class__.inspect_c1["Mounts"][0]["Source"], "/Users/mac/Desktop/CS527/cs527project/volume")
+        self.assertEqual(self.__class__.inspect_c1["Mounts"][0]["Destination"], "/opt/myapp")
+
+
+
 
 
 if __name__ == '__main__':
